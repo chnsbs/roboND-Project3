c584cad541ef65312f3f2716a1a27f24ef91f6f2
chnsbs <cihansubasi.tr@gmail.com>
2022-12-06 02:02:04 -0800

    created a new robot model  and a new worldfile

--- script/hello.cpp
+++ script/hello.cpp
@@ -5,6 +5,7 @@ namespace gazebo{
     class WorldPLuginMyRobot : public WorldPlugin{
         public: WorldPLuginMyRobot():WorldPlugin(){
             printf("Welcome to Cihan's World!\n");
+            
         }
 
         public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf){
